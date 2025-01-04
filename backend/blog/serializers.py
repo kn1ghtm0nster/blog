492ed8550d39ccca0695260fe9f37a6161104da0
Diego Quintanilla <diego.quintanilla@proton.me>
2025-01-04 17:48:40 -0500

    add serializer and route for starting blog api route

---
+++ backend/blog/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+from .models import BlogPost, Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    """Serializer for Comment model"""
+
+    author = serializers.ReadOnlyField(source='author.username')
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'author', 'author', 'content', 'parent',
+                  'created_at', 'updated_at', 'is_moderated']
+
+
+class BlogPostSerializer(serializers.ModelSerializer):
+    """Serializer for BlogPost model"""
+
+    author = serializers.ReadOnlyField(source='author.username')
+    comments = CommentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = BlogPost
+        fields = ['id', 'title', 'content', 'author',
+                  'allow_comments', 'created_at', 'updated_at', 'comments']
--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+from .models import BlogPost
+from .serializers import BlogPostSerializer
+
+
+class BlogPostViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing blog post instances
+    """
+
+    serializer_class = BlogPostSerializer
+    queryset = BlogPost.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)

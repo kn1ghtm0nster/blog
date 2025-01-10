a77e00397768f3a89615bf7f791dd3696811d2ec
Diego Quintanilla <diego.quintanilla@proton.me>
2025-01-10 18:55:51 -0500

    add register view class to auth_app

--- backend/auth_app/views.py
+++ backend/auth_app/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from .serializers import RegisterSerializer, UserSerializer
+
+
+class RegisterUserApi(APIView):
+    """
+    API view for user registration
+    """
+
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.save()
+            user_serializer = UserSerializer(user)
+
+            # Generate JWT token
+            refresh = RefreshToken.for_user(user)
+            token = str(refresh.access_token)
+
+            # Prep response data to include JWT token
+            response_data = user_serializer.data
+            response_data['token'] = token
+
+            return Response(response_data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

be7ade954892246f4480d1170cdc6fecfe1fb6a0
Diego Quintanilla <diego.quintanilla@proton.me>
2025-01-12 16:55:21 -0500

    update auth view test suite and implement remaining tests

--- backend/auth_app/tests/test_auth_views.py
+++ backend/auth_app/tests/test_auth_views.py
@@ -1,37 +1,143 @@
 import pytest
 from django.urls import reverse
+from django.contrib.auth.models import User
+from rest_framework import status
 
 
 @pytest.mark.django_db
-def test_register_user_success(client):
-    url = reverse('register')
-    data = {
-        'username': 'user123',
-        'email': 'user@example.com',
-        'password': 'Password123!',
-        'password2': 'Password123!'
-    }
-    response = client.post(url, data, format='json')
+class TestRegisterUserApi:
+    def test_register_user_successfully(self, client):
+        url = reverse('register')
+        data = {
+            'username': 'user123',
+            'email': 'user@example.com',
+            'password': 'Password123!',
+            'password2': 'Password123!'
+        }
+        response = client.post(url, data, format='json')
 
-    assert response.status_code == 201
-    assert 'token' in response.json()
+        assert response.status_code == status.HTTP_201_CREATED
+        # Check the response data
+        assert 'id' in response.json()
+        assert response.json()['username'] == 'user123'
+        assert response.json()['email'] == 'user@example.com'
+        assert response.json()['admin'] is False
+        assert 'token' in response.json()
+
+    def test_register_duplicate_username(self, client, create_user):
+        # Create initial user
+        create_user(username='user123', email='user123@example.com',
+                    password='Password@123')
+        url = reverse('register')
+        data = {
+            'username': 'user123',
+            'email': 'new_email@exampl.com',
+            'password': 'NewPassword@123',
+            'password2': 'NewPassword@123'
+        }
+        response = client.post(url, data, format='json')
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert 'username' in response.json()
+
+    def test_register_duplicate_email(self, client, create_user):
+        create_user(username="user1",
+                    email="uniqueemail@example.com", password="Pass123!")
+        url = reverse('register')
+        data = {
+            "username": "user2",
+            "email": "uniqueemail@example.com",
+            "password": "Pass456!",
+            "password2": "Pass456!"
+        }
+        response = client.post(url, data, format='json')
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert "email" in response.json()
+
+    def test_register_password_mismatch(self, client):
+        url = reverse('register')
+        data = {
+            'username': 'user123',
+            'email': 'user3@example.com',
+            'password': 'Pass123!',
+            'password2': 'Pass1234!'
+        }
+        response = client.post(url, data, format='json')
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert 'password' in response.json()
+
+    def test_register_weak_password(self, client):
+        url = reverse('register')
+        data = {
+            'username': 'user123',
+            'email': 'weakpass@example.com',
+            'password': 'short',
+            'password2': 'short'
+        }
+        response = client.post(url, data, format='json')
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert 'password' in response.json()
 
 
 @pytest.mark.django_db
-def test_register_duplicate_username(client, create_user):
-    # Create initial user
-    create_user(username='user123', email='user123@example.com',
-                password='Password@123')
+class TestObtainTokenApi:
+    def test_obtain_token_successfully(self, client, create_user):
+        create_user(username='tokenuser', email='token@example.com',
+                    password='TokenPassword123!')
+        url = reverse('token_obtain')
+        data = {
+            'username': 'tokenuser',
+            'password': 'TokenPassword123!'
+        }
+        response = client.post(url, data, format='json')
 
-    url = reverse('register')
-    data = {
-        'username': 'user123',
-        'email': 'new_email@exampl.com',
-        'password': 'NewPassword@123',
-        'password2': 'NewPassword@123'
-    }
+        assert response.status_code == status.HTTP_200_OK
+        assert 'token' in response.json()
 
-    response = client.post(url, data, format='json')
+    def test_obtain_token_invalid_username(self, client, create_user):
+        create_user(username='tokenuser', email='token@example.com',
+                    password='TokenPassword123!')
+        url = reverse('token_obtain')
+        data = {
+            'username': 'invaliduser',
+            'password': 'TokenPassword123!'
+        }
+        response = client.post(url, data, format='json')
 
-    assert response.status_code == 400
-    assert 'username' in response.json()
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert 'non_field_errors' in response.json()
+
+    def test_obtain_token_invalid_password(self, client, create_user):
+        create_user(username='tokenuser', email='token@example.com',
+                    password='TokenPassword123!')
+        url = reverse('token_obtain')
+        data = {
+            'username': 'tokenuser',
+            'password': 'InvalidPassword123!'
+        }
+        response = client.post(url, data, format='json')
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert 'non_field_errors' in response.json()
+
+    def test_obtain_token_missing_username(self, client):
+        url = reverse('token_obtain')
+        data = {
+            'password': 'TokenPassword'
+        }
+        response = client.post(url, data, format='json')
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert 'username' in response.json()
+
+    def test_obtain_token_missing_password(self, client):
+        url = reverse('token_obtain')
+        data = {
+            'username': 'tokenuser'
+        }
+        response = client.post(url, data, format='json')
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert 'password' in response.json()
